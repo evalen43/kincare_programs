ea8821f8dcaea03968891d503858ff0bbe9820b6
Ernesto Valenzuela <edvallen@hotmail.com>
2022-08-08 16:24:07 -0500

    save changes 8/8/22 4:23 pm

--- hours_monthly.py
+++ hours_monthly.py
@@ -14,26 +14,30 @@ clients=[]
 df=pd.read_excel(files[0],sheet_name='summary',header=None,usecols='A:N')
 row=df.loc[1:1]
 provider=df.loc[1:1].values.flatten().tolist()
-provider[0]='Names'
-#print(provider)
-#print(df.head(4))
-#provider_hours=[]
+provider[0]='Clients'
+
 for file in files:
     df=pd.read_excel(file,sheet_name='summary',header=None,usecols='A:N')
     client=df.loc[14:14].values.flatten().tolist()
     clients.append(client)
-df2=pd.DataFrame(clients) 
-print (df2)   
+df2=pd.DataFrame(data=clients,columns=provider) 
+#print (df2) 
+hours_provider=df2.sum(axis=0,numeric_only=True)
+#hours_clients=df2.sum(axis=1,numeric_only=True)
+pd.options.display.float_format='{:.2f}'.format
+#print(f"{total_hours:.2f}")
+print(hours_provider)
+#print(hours_clients)
 # for client in clients:    
 #     print(client)
-for j in range(2,14):
-    hours=0.
-    for i in range(len(clients)):
-        hours +=clients[i][j]
-    if (provider[j] ==' ') or (hours==0.0) :
-        continue 
-    else:   
-        print((provider[j]+"\t"+f"{hours:.2f}").expandtabs(15))
+# for j in range(2,14):
+#     hours=0.
+#     for i in range(len(clients)):
+#         hours +=clients[i][j]
+#     if (provider[j] ==' ') or (hours==0.0) :
+#         continue 
+#     else:   
+#         print((provider[j]+"\t"+f"{hours:.2f}").expandtabs(15))
         
             
 
