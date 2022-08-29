aebd5078d4573bd339e44eb863242dd2d04dd0ca
Ernesto Valenzuela <edvallen@hotmail.com>
2022-08-29 09:31:41 -0500

    save changes 8/29/22 9:31 am

--- hours_monthly.py
+++ hours_monthly.py
@@ -3,7 +3,10 @@ from weakref import ReferenceType
 #import openpyxl
 import pandas as pd
 
-txt_files=open('clients_july-august.txt',"r")
+per1=4
+per2=14
+
+txt_files=open('clients_august.txt',"r")
 files=txt_files.read().splitlines()
 #print(files)
 provider=[]
@@ -15,14 +18,16 @@ provider[0]='Clients'
 
 for file in files:
     df=pd.read_excel(file,sheet_name='summary',header=None,usecols='A:N')
-    client=df.loc[14:14].values.flatten().tolist()
+    client=df.loc[per1:per1].values.flatten().tolist()
     clients.append(client)
 df2=pd.DataFrame(data=clients,columns=provider) 
-#print (df2) 
+print (df2)
+# df3=df2.drop('     ') 
 hours_provider=df2.sum(axis=0,numeric_only=True)
 #hours_clients=df2.sum(axis=1,numeric_only=True)
 pd.options.display.float_format='{:.2f}'.format
-#print(f"{total_hours:.2f}")
+#print(provider)
+
 print(hours_provider)
 #print(hours_clients)
 # for client in clients:    
