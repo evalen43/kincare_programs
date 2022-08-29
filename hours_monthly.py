9a98dacde858a6c8905393588693d4e4f4b07259
Ernesto Valenzuela <edvallen@hotmail.com>
2022-08-29 15:12:36 -0500

    save changes 8/29/22 3:12 pm

--- hours_monthly.py
+++ hours_monthly.py
@@ -6,10 +6,10 @@ import numpy as np
 num=input("Input 1 or 2 for Period1 or Period2: ")
 print(num)
 if num == '1':
-    per1=4
+    period=4
     print("You selected a Summary for Period1")
 else: 
-    per1=14
+    period=14
     print("You selected a Summary for Period2")
 
 txt_files=open('clients_august.txt',"r")
@@ -24,31 +24,26 @@ provider[0]='Clients'
 
 for file in files:
     df=pd.read_excel(file,sheet_name='summary',header=None,usecols='A:N')
-    client=df.loc[per1:per1].values.flatten().tolist()
+    client=df.loc[period:period].values.flatten().tolist()
     clients.append(client)
-df2=pd.DataFrame(data=clients,columns=provider) 
-#df2 = df2.loc[:, df2.all()]
-#df2 = df2.replace({0: pd.NA})
-#print(df2)
-#df3=df2.dropna(how='all',axis=1,inplace=False) 
-print (df2)
-hours_provider=df2.sum(axis=0,numeric_only=True)
-#hours_clients=df2.sum(axis=1,numeric_only=True)
-pd.options.display.float_format='{:.2f}'.format
-#print(provider)
 
-print(hours_provider)
-#print(hours_clients)
-# for client in clients:    
-#     print(client)
-# for j in range(2,14):
-#     hours=0.
-#     for i in range(len(clients)):
-#         hours +=clients[i][j]
-#     if (provider[j] ==' ') or (hours==0.0) :
-#         continue 
-#     else:   
-#         print((provider[j]+"\t"+f"{hours:.2f}").expandtabs(15))
+df2=pd.DataFrame(data=clients,columns=provider) 
+
+#print (df2)
+hours_provider=df2.sum(axis=0,numeric_only=True)
+hours_clients=df2.sum(axis=1,numeric_only=True)
+pd.options.display.float_format='{:.2f}'.format
+print(hours_clients)
+
+
+for j in range(2, len(provider)):
+    hours=0.
+    for i in range(len(clients)):
+        hours +=clients[i][j]
+    if (provider[j] ==' ') or (hours==0.0) :
+        continue 
+    else:   
+        print((provider[j]+"\t"+f"{hours:.2f}").expandtabs(15))
         
             
 
